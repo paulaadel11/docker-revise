8d3ee7907d45f10c3b405887013a85c9a1384fc6
paulaadel11 <polaadel1705@gmail.com>
2026-06-27 13:53:14 +0300

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders app text', () => {
+  render(<App />);
+  const textElement = screen.getByText(/i luv sandoo/i);
+  expect(textElement).toBeInTheDocument();
+});
+
