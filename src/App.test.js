83e56933e0e810a914b1542f073469d8c96d8ce3
paulaadel11 <polaadel1705@gmail.com>
2026-07-06 14:53:22 +0300

    revise-pipeline2

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,14 @@ test('renders app text', () => {
   const textElement = screen.getByText(/i luv sandoo/i);
   expect(textElement).toBeInTheDocument();
 });
+test('renders app text', () => {
+  render(<App />);
+  const textElement = screen.getByText(/i luv sandoo/i);
+  expect(textElement).toBeInTheDocument();
+});
+test('renders app text', () => {
+  render(<App />);
+  const textElement = screen.getByText(/i luv sandoo/i);
+  expect(textElement).toBeInTheDocument();
+});
 
